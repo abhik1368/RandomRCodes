b5e653b6f36792dfd9cd5a6b07d650d306780185
Abhik Seal <abhik1368@gmail.com>
2014-03-04 22:52:54 -0500

    Plots using R

--- Scaffold.R
+++ Scaffold.R
@@ -45,7 +45,38 @@ heatmap.2(as.matrix(depvs[,2:50]), dendrogram="col",col=redgreen(75),cexRow=0.9,
 rownames(depvs)<-depvs[,1]
 pheatmap(as.matrix(depvs[,2:50]),show_rownames=T,color = colorRampPalette(c("navy", "white", "firebrick3"))(50))
 
+#-------------------------------------------------------#
+# Working with full dataset and matched molecular pairs #
+# taken from Rajarshi's code                            #
+#-------------------------------------------------------#
+#read in the test dataset of compounds and targets
+data<-read.csv("testKinase.csv",header=T)
 
-##########
+#parse the smiles from the compounds
+Smiles<-lapply(as.character(data[,2]),parse.smiles)
+fps<-lapply(Smiles,get.fingerprint,'extended')
+cmp.fp = vector("list",length(Smiles))
+
+for (i in 1:length(Smiles)){
+  
+  cmp.fp[[i]] = lapply(Smiles[[i]], get.fingerprint, type="extended")
+  
+}
+fp.sim = fp.sim.matrix(unlist(cmp.fp))
+idxs<-which(fp.sim > 0.95,arr.ind=TRUE)
+idxs <- idxs[idxs[,1] > idxs[,2],]
+
 rownames(data)<-data[,1]
-da
+data[is.na(data)]<-0
+
+#matched molecular pairs
+mps<-t(apply(idxs,1,function(x){
+  apply(data,2,function(z){
+    d<-abs(z[x[1]]-z[x[2]])
+    ifelse(d >=1,d,NA)
+  })  
+}))
+dim(mps)
+mps[is.na(mps)]<-0
+rownames(mps)<-mps[,1]
+pheatmap(mps[1:20,2:100],cluster_rows=FALSE,color=colorRampPalette(rev(c("#D73027", "#FC8D59", "#FEE090", "#FFFFBF", "#E0F3F8", "#91BFDB", "#4575B4")))(100),border_color="black",fontsize=12)
