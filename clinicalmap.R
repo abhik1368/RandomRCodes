36845488cce6655e90be72b077852c53e2abd5f6
abhik1368 <abhik1368@gmail.com>
2015-01-17 16:26:56 -0500

    update code

--- clinicalmap.R
+++ clinicalmap.R
@@ -7,11 +7,12 @@ library(rMaps)
 library(geomapdata)
 library(rgdal)
 library(RgoogleMaps)
-library(ggmap)
+library(httr)
+library(rjson)
 
 # Define search term
 keyword      <- "tuberculosis"
-start_date   <- "1/1/2013"
+start_date   <- "1/1/2009"
 stop_date    <- "12/31/2014"
 trial_status <- "Closed"
 
@@ -54,7 +55,15 @@ for (i in 1:length(file.list))
 location_tab           <- as.data.frame(location_tab)
 colnames(location_tab) <- c("trialId", "city", "country")
 
+# Tried this API service looks like it cannot access the other countries locations
 # Get longitude and latitude coordinates
+# location_tab$address<-paste(location_tab$city,",",location_tab$country)
+# data <- paste0("[",paste(paste0("\"",location_tab$address,"\""),collapse=","),"]")
+# url  <- "http://www.datasciencetoolkit.org/street2coordinates"
+# response <- POST(url,body=data)
+# json     <- fromJSON(content(response,type="text"))
+# geocode  <- do.call(rbind,sapply(json,function(x) c(long=x$longitude,lat=x$latitude)))
+
 
 get_geocode         <- geocode(as.character(location_tab[,2]))
 location_tab$long   <- as.numeric(get_geocode[,1])
@@ -64,14 +73,17 @@ location_tab$lat    <- as.numeric(get_geocode[,2])
 map <- Leaflet$new()
 map$setView(c(33.320731,-14.033990), zoom = 2)
 map$tileLayer(provider = 'Stamen.Toner')
-
+map$set(width = 1600, height =800)
 for (i in 1:nrow(location_tab))
 {
   if(is.na(location_tab$lat[i]) | is.na(location_tab$lon[i])) {next}
   map$marker(
     c(location_tab$lat[i], location_tab$lon[i]),
-    bindPopup = location_tab$trial_id[i])
+    bindPopup = location_tab$trialId[i])
   
 }  
-map
 
+map
+sink("outfile.txt")
+map$show('iframesrc',cdn=TRUE)
+sink()
